5e0796324bd5422abefb352752cd288e0641743b
balintmolnar21 <molnar.balint.otto@gmail.com>
2026-02-18 11:26:56 +0100

    Second repair

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[N_ELEMENTS];
-    std::cout << "1-100 ertekek duplazasa" ;
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
     for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
