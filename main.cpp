7522982dc5f8abac8804c3a8fadee71db33bc065
balintmolnar21 <molnar.balint.otto@gmail.com>
2026-02-19 20:00:27 +0100

    Repair comments

--- main.cpp
+++ main.cpp
@@ -4,18 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    //NELEMENTS  N_ELEMENTS helyett
     int *b = new int[N_ELEMENTS];
+    //' helyett " kell mivel string
     std::cout << "1-100 ertekek duplazasa" << std::endl;
+    // Ciklusfeltétel hiányzott, i < N_ELEMENTS kell
     for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        //i = 0 indul ezért kell az i+1, hogy 1-100 legyen
+        b[i] = (i+1) * 2;
     };
-    for (int i = 0; i; i++)
+    // Hiányzott i< N_ELEMENTS feltétel
+    for (int i = 0; i< N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:";
+        // Hiányzik a kiírandó érték
+        std::cout << "Ertek:" << b[i] << std::endl;
     };
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    // Átlagot 0-ként kell inicializálni és legyen double típusú, hogy ne legyen egész osztás
+    double atlag = 0.0;
+    // Vessző helyett pontosvesszőt kell tenni
     for (int i = 0; i < N_ELEMENTS; i++)
     {
         atlag += b[i];
@@ -23,4 +31,6 @@ int main()
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
+
+    // A kód több pontján hiányoztak a sorzáró pontosvesszők
 }
